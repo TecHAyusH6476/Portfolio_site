edb2a78700df597fd75071dd3812d0e6366ed114
manikumar.perla <manikumar.perla@linecorp.com>
2020-04-23 21:45:00 +0900

    Module 6 - Lesson 8

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Title from "./components/Title";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import ProjectSection from "./components/ProjectSection";
+import BlogSection from "./components/BlogSection";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Portfolio website</h1>
+    <div>
+      <Navbar />
+      <Title name="Manikumar Perla" leadText="I am a freelancer from India" />
+      <ProjectSection />
+      <About />
+      <BlogSection />
+      <Footer />
     </div>
   );
 }
